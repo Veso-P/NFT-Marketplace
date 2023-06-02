fe4cddbb529ae54b10113dd5bfbdc9a839176f25
Veso-P <vesko_petkov@abv.bg>
2023-06-02 15:24:45 +0300

    createAuction function implementation

--- Homeworks/GitHub/AuctionPlatform.sol
+++ Homeworks/GitHub/AuctionPlatform.sol
@@ -17,4 +17,55 @@ contract AuctionPlatform {
     uint256 id = 0; 
     mapping(uint256 => Auction) auctions;
 
+    // Events:
+    event NewAuction(
+        address indexed creator,
+        uint256 indexed identifier,
+        string name,
+        string description,
+        uint256 starTime,
+        uint256 endTime,
+        uint256 startingPrice
+    );
+
+    // (1) A function to create a new auction.
+    function createAuction(
+        uint256 startsIn,
+        uint32 duration,
+        string memory name,
+        string memory description,
+        uint256 startingPrice
+    ) external {
+        require(
+            startsIn >= 30 seconds,
+            "It should start in at least 30 seconds!"
+        );
+
+        require(
+            duration >= 2 minutes,
+            "The duration should be at least 120 seconds (2 minutes)!"
+        );
+
+        Auction storage newAuction = auctions[id];
+
+        newAuction.startTime = block.timestamp + startsIn;
+        newAuction.endTime = block.timestamp + startsIn + duration;
+        newAuction.name = name;
+        newAuction.description = description;
+        newAuction.price = startingPrice;
+        newAuction.auctionCreator = msg.sender;
+
+        emit NewAuction(
+            msg.sender,
+            id,
+            name,
+            description,
+            block.timestamp + startsIn,
+            block.timestamp + startsIn + duration,
+            startingPrice
+        );
+
+        id++;
+    }
+
 }
