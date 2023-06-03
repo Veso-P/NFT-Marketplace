f2ff8231d23673ae105c1d422ccb1796ee89f975
Veso-P <vesko_petkov@abv.bg>
2023-06-03 17:45:57 +0300

    placeBid function and onlyActiveAuction modifier implementations

--- Homeworks/GitHub/AuctionPlatform.sol
+++ Homeworks/GitHub/AuctionPlatform.sol
@@ -16,6 +16,7 @@ contract AuctionPlatform {
      // Storage variables:
     uint256 id = 0; 
     mapping(uint256 => Auction) auctions;
+    mapping(address => uint256) availableToWithdrawal;
 
     // Events:
     event NewAuction(
@@ -27,6 +28,14 @@ contract AuctionPlatform {
         uint256 endTime,
         uint256 startingPrice
     );
+
+     event NewHighestBid(        
+        uint256 indexed identifier,
+        address previousHighestBidder,
+        address newHighestBidder,
+        uint256 bid
+    );
+
 
     // (1) A function to create a new auction.
     function createAuction(
@@ -66,6 +75,47 @@ contract AuctionPlatform {
         );
 
         id++;
+    }
+
+    // (2) A function used to place a bid on an auction.
+    function placeBid(uint256 auctionId)
+        external
+        payable
+        onlyActiveAuction(auctionId)
+    {
+        require(msg.sender != auctions[auctionId].auctionCreator, "No bids on your own auction!");
+        require(msg.sender != auctions[auctionId].highestBidder, "You have already placed the highest bid!");
+        uint256 bid = msg.value;
+        require(
+            bid > auctions[auctionId].price,
+            "Place a bid higher than the current one!"
+        );
+
+        address previousHighestBidder = auctions[auctionId].highestBidder;
+        uint256 prevousHighestBid = auctions[auctionId].price;
+
+        availableToWithdrawal[previousHighestBidder] += prevousHighestBid;
+        auctions[auctionId].price = bid; // The new higehst bid for the auction.         
+        auctions[auctionId].highestBidder = msg.sender; // The new highest bidder for the auction.
+
+        emit NewHighestBid (auctionId, previousHighestBidder, auctions[auctionId].highestBidder, bid );
+    
+    }
+
+    // MODIFIERS
+    // (5) A modifier used to check if the auction is active. 
+    modifier onlyActiveAuction (uint256 index) {
+        require(
+            block.timestamp >= auctions[index].startTime,
+            "The auction has not started!"
+        );
+
+        require(
+            block.timestamp <= auctions[index].endTime,
+            "The auction has finished!"
+        );
+       
+        _;
     }
 
 }
